b7def58b7366037cbcb441759795bd39a6658c0d
Jonas Neves <72263638+jonasneves@users.noreply.github.com>
2026-02-08 13:40:28 -0500

    Simplify base path and add ML dependencies (#38)

--- webapp-react/vite.config.ts
+++ webapp-react/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') }
   },
-  base: process.env.NODE_ENV === 'production' ? '/main/' : '/',
+  base: '/',
   build: { outDir: 'dist', sourcemap: false }
 })
